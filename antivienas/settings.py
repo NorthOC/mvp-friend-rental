5b202b995605cde6c9651ef0ab267776cdd35911
NorthOC <rev.denisas@gmail.com>
2024-05-20 16:38:40 +0300

    test

--- antivienas/settings.py
+++ antivienas/settings.py
@@ -81,9 +81,16 @@ WSGI_APPLICATION = 'antivienas.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'H0NnEuB8bTXl',
+        'HOST': 'ep-flat-poetry-a2c5jup9-pooler.eu-central-1.aws.neon.tech',
     }
 }
 
@@ -126,12 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 MEDIA_URL = 'media/'
-
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
